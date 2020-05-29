37f731cc20ad96ef54a71ddf6d8b49fc2540f3ee
Eduardo Vieira <eduardovra@gmail.com>
2020-05-28 23:35:17 -0300

    Create asteriods app

---
+++ asteroids/main.c
@@ -0,0 +1,250 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <math.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <cglm/cglm.h>
+
+#include "shader.h"
+#include "mesh.h"
+#include "model.h"
+#include "light_cube_vertices.h"
+
+#define SCR_WIDTH 800
+#define SCR_HEIGHT 600
+
+vec3 cameraPos = {0.0f, 0.0f, 3.0f};
+vec3 cameraFront = {0.0f, 0.0f, -1.0f};
+vec3 cameraUp = {0.0f, 1.0f, 0.0f};
+
+bool firstMouse = true;
+float yaw   = -90.0f; // yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX = SCR_WIDTH / 2, lastY = SCR_HEIGHT / 2;
+float fov   =  45.0f;
+
+float deltaTime = 0.0f;	// Time between current frame and last frame
+float lastFrame = 0.0f; // Time of last frame
+
+vec3 lightPos = {1.2f, 1.0f, 2.0f};
+
+void error_callback (int error, const char* description)
+{
+    printf("%s\n", description);
+}
+
+void framebuffer_size_callback (GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput (GLFWwindow *window)
+{
+    float cameraSpeed = 2.5f * deltaTime;
+    vec3 tmp, tmp2;
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+        glm_vec3_scale(cameraFront, cameraSpeed, tmp);
+        glm_vec3_add(cameraPos, tmp, cameraPos);
+    }
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+        glm_vec3_scale(cameraFront, cameraSpeed, tmp);
+        glm_vec3_sub(cameraPos, tmp, cameraPos);
+    }
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+        glm_vec3_cross(cameraFront, cameraUp, tmp);
+        glm_vec3_normalize(tmp);
+        glm_vec3_scale(tmp, cameraSpeed, tmp2);
+        glm_vec3_sub(cameraPos, tmp2, cameraPos);
+    }
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+        glm_vec3_cross(cameraFront, cameraUp, tmp);
+        glm_vec3_normalize(tmp);
+        glm_vec3_scale(tmp, cameraSpeed, tmp2);
+        glm_vec3_add(cameraPos, tmp2, cameraPos);
+    }
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+}
+
+void mouse_callback (GLFWwindow* window, double xpos, double ypos)
+{
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates range from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    const float sensitivity = 0.05f;
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f) {
+        pitch = 89.0f;
+    }
+    if (pitch < -89.0f) {
+        pitch = -89.0f;
+    }
+
+    float x = cos(glm_rad(yaw)) * cos(glm_rad(pitch));
+    float y = sin(glm_rad(pitch));
+    float z = sin(glm_rad(yaw)) * cos(glm_rad(pitch));
+    vec3 direction = {x, y, z};
+    glm_vec3_normalize_to(direction, cameraFront);
+}
+
+void scroll_callback (GLFWwindow* window, double xoffset, double yoffset)
+{
+    if (fov >= 1.0f && fov <= 45.0f) {
+        fov -= yoffset;
+    }
+    if (fov < 1.0f) {
+        fov = 1.0f;
+    }
+    if (fov > 45.0f) {
+        fov = 45.0f;
+    }
+}
+
+GLFWwindow* createWindow ()
+{
+    glfwSetErrorCallback(error_callback);
+
+    if (glfwInit() == GLFW_FALSE) {
+        printf("Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+    if (window == NULL) {
+        printf("Failed to create GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc) glfwGetProcAddress)) {
+        printf("Failed to initialize GLAD\n");
+        exit(EXIT_FAILURE);
+    }
+
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+
+    return window;
+}
+
+int main (int argc, char *argv[])
+{
+    GLFWwindow *window = createWindow();
+
+    glEnable(GL_DEPTH_TEST);
+
+    // tell stb_image.h to flip loaded texture's on the y-axis (before loading model).
+    stbi_set_flip_vertically_on_load(true);
+
+    unsigned int program = createProgram("model_loading/shader.vert", "model_loading/shader.frag");
+    unsigned int lightProgram = createProgram("model_loading/light_shader.vert", "model_loading/light_shader.frag");
+    Model model = createModel("resources/backpack/backpack.obj");
+
+    // configure light cube
+    unsigned int VBO, lightCubeVAO;
+    glGenVertexArrays(1, &lightCubeVAO);
+    glGenBuffers(1, &VBO);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindVertexArray(lightCubeVAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) 0);
+    glEnableVertexAttribArray(0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        float currentFrame = glfwGetTime();
+        deltaTime = currentFrame - lastFrame;
+        lastFrame = currentFrame;
+
+        processInput(window);
+
+        glClearColor(0.05f, 0.05f, 0.05f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(program);
+
+        // wireframe mode
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+        // light properties
+        glUniform3fv(glGetUniformLocation(program, "viewPos"), 1, cameraPos);
+        glUniform3fv(glGetUniformLocation(program, "light.position"), 1, lightPos);
+        vec3 lightAmbient = {0.2f, 0.2f, 0.2f};
+        vec3 lightDiffuse = {0.5f, 0.5f, 0.5f};
+        vec3 lightSpecular = {1.0f, 1.0f, 1.0f};
+        glUniform3fv(glGetUniformLocation(program, "light.ambient"), 1, lightAmbient);
+        glUniform3fv(glGetUniformLocation(program, "light.diffuse"), 1, lightDiffuse);
+        glUniform3fv(glGetUniformLocation(program, "light.specular"), 1, lightSpecular);
+
+        // view/projection transformations
+        mat4 view, projection;
+        vec3 center;
+        glm_vec3_add(cameraPos, cameraFront, center);
+        glm_lookat(cameraPos, center, cameraUp, view);
+        glm_perspective(glm_rad(fov), (float) SCR_WIDTH / (float) SCR_HEIGHT, 0.1f, 100.0f, projection);
+        glUniformMatrix4fv(glGetUniformLocation(program, "view"), 1, GL_FALSE, (float *) view);
+        glUniformMatrix4fv(glGetUniformLocation(program, "projection"), 1, GL_FALSE, (float *) projection);
+
+        // render the loaded model
+        mat4 modelMatrix;
+        glm_mat4_identity(modelMatrix);
+        vec3 auxTranslate = {0.0f, 0.0f, 0.0f};
+        glm_translate(modelMatrix, auxTranslate);
+        vec3 auxScale = {1.0f, 1.0f, 1.0f};
+        glm_scale(modelMatrix, auxScale);
+        glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, (float *) modelMatrix);
+        drawModel(&model, program);
+
+        // draw point light
+        glUseProgram(lightProgram);
+        glUniformMatrix4fv(glGetUniformLocation(lightProgram, "view"), 1, GL_FALSE, (float *) view);
+        glUniformMatrix4fv(glGetUniformLocation(lightProgram, "projection"), 1, GL_FALSE, (float *) projection);
+        glm_mat4_identity(modelMatrix);
+        glm_translate(modelMatrix, lightPos);
+        vec3 lightCubeSize = {0.2f, 0.2f, 0.2f};
+        glm_scale(modelMatrix, lightCubeSize);
+        glUniformMatrix4fv(glGetUniformLocation(program, "model"), 1, GL_FALSE, (float *) modelMatrix);
+
+        glBindVertexArray(lightCubeVAO);
+        glDrawArrays(GL_TRIANGLES, 0, 36);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    //glDeleteVertexArrays(1, &cubeVAO);
+    //glDeleteVertexArrays(1, &lightVAO);
+    //glDeleteBuffers(1, &VBO);
+    glDeleteProgram(program);
+    glDeleteProgram(lightProgram);
+
+    glfwTerminate();
+
+    return EXIT_SUCCESS;
+}
