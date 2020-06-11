b85825b404fa4a2e1a8cce5cd60116b73d10002c
Eduardo Vieira <eduardovra@gmail.com>
2020-06-11 14:51:15 -0300

    Drawing board

--- tetris/main.c
+++ tetris/main.c
@@ -23,6 +23,9 @@
 #define SCR_WIDTH 800
 #define SCR_HEIGHT 600
 
+#define BOARD_ROWS 12
+#define BOARD_COLS 22
+
 Camera camera;
 
 float lastX = SCR_WIDTH / 2.0f;
@@ -35,14 +38,26 @@ float lastFrame = 0.0f; // Time of last frame
 //struct ImGuiContext *ctx;
 //struct ImGuiIO *io;
 
-vec3 colorGrey = {0.5f, 0.5f, 0.5f};
-vec3 colorRed = {1.0f, 0.0f, 0.0f};
-vec3 colorGreen = {0.0f, 1.0f, 0.0f};
-vec3 colorBlue = {0.0f, 0.0f, 1.0f};
+typedef enum {
+    COLOR_BLACK = 0,
+    COLOR_GREY = 1,
+    COLOR_RED = 2,
+    COLOR_GREEN = 3,
+    COLOR_BLUE = 4,
+} colors;
+
+vec3 colorsDef[] = {
+    {0.0f, 0.0f, 0.0f}, // black
+    {0.5f, 0.5f, 0.5f}, // grey
+    {1.0f, 0.0f, 0.0f}, // red
+    {0.0f, 1.0f, 0.0f}, // green
+    {0.0f, 0.0f, 1.0f}, // blue
+};
 
 vec2 currentCubePos = {0.0f, 10.0f};
 float cubeSpeed = 0.2f;
 int rotation = 0;
+unsigned int board[BOARD_ROWS][BOARD_COLS];
 
 void error_callback (int error, const char *description)
 {
@@ -168,7 +183,7 @@ GLFWwindow* createWindow ()
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    //glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
 
@@ -178,7 +193,7 @@ GLFWwindow* createWindow ()
 void drawCube (unsigned int program, float x, float y, float *color)
 {
     mat4 model;
-    vec3 pos = {x, y, 0.0f};
+    vec3 pos = {x - (BOARD_ROWS / 2), y - (BOARD_COLS / 2), 0.0f};
 
     glm_mat4_identity(model);
     glm_translate(model, pos);
@@ -187,7 +202,7 @@ void drawCube (unsigned int program, float x, float y, float *color)
 
     glDrawArrays(GL_TRIANGLES, 0, 36);
 }
-
+/*
 void drawBoard (unsigned int program)
 {
     // 12 x 22 colunas
@@ -199,6 +214,36 @@ void drawBoard (unsigned int program)
     for (float y = -11.0f; y <= 11.0f; y++) {
         drawCube(program, -6.0f, y, colorGrey);
         drawCube(program, 5.0f, y, colorGrey);
+    }
+}
+*/
+void initBoard ()
+{
+    memset(board, COLOR_BLACK, sizeof(board));
+
+    // lateral columns
+    for (unsigned int x = 0; x < BOARD_ROWS; x++) {
+        board[x][0] = COLOR_GREY;
+        board[x][BOARD_COLS - 1] = COLOR_GREY;
+    }
+
+    // upper and bottom rows
+    for (unsigned int y = 0; y < BOARD_COLS; y++) {
+        board[0][y] = COLOR_GREY;
+        board[BOARD_ROWS - 1][y] = COLOR_GREY;
+    }
+}
+
+void renderBoard (unsigned int program)
+{
+    for (unsigned int x = 0; x < BOARD_ROWS; x++) {
+        for (unsigned int y = 0; y < BOARD_COLS; y++) {
+            unsigned int piece = board[x][y];
+            if (piece) {
+                vec3 color = {0.5f, 0.5f, 0.5f};
+                drawCube(program, (float) x, (float) y, color);
+            }
+        }
     }
 }
 
@@ -250,6 +295,8 @@ int main (int argc, char *argv[])
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void *) 0);
     glEnableVertexAttribArray(0);
 
+    initBoard();
+
     while (!glfwWindowShouldClose(window))
     {
         float currentFrame = glfwGetTime();
@@ -275,24 +322,7 @@ int main (int argc, char *argv[])
         glUseProgram(lightProgram);
         glBindVertexArray(lightCubeVAO);
 
-        drawBoard(lightProgram);
-        drawCube(lightProgram, -5.0f, -10.0f, colorRed);
-        drawCube(lightProgram, -4.0f, -10.0f, colorRed);
-        drawCube(lightProgram, -3.0f, -10.0f, colorRed);
-        drawCube(lightProgram, -2.0f, -10.0f, colorGreen);
-        drawCube(lightProgram, -1.0f, -10.0f, colorRed);
-        drawCube(lightProgram, 0.0f, -10.0f, colorRed);
-        drawCube(lightProgram, 1.0f, -10.0f, colorRed);
-        drawCube(lightProgram, 2.0f, -10.0f, colorBlue);
-        drawCube(lightProgram, 3.0f, -10.0f, colorRed);
-        drawCube(lightProgram, 4.0f, -10.0f, colorRed);
-
-        currentCubePos[1] -= cubeSpeed * deltaTime * glfwGetTime();
-        if (currentCubePos[1] < -9.0f) {
-            currentCubePos[1] = -9.0f;
-        }
-
-        drawTetromino(lightProgram, 2, rotation, currentCubePos[0], currentCubePos[1], colorBlue);
+        renderBoard(lightProgram);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
